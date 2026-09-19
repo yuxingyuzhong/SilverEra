93587e1c5f9a6e910c9a0f7e50bab92095575ce5
行雨中 雨 <2644320744@qq.com>
2026-09-19 10:29:05 +0800

    移除误入快照的编译辅助脚本

--- build_cmd.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "D:\ViusalStudioCommunity2026\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.51.36231 >nul 2>&1
-"D:\ViusalStudioCommunity2026\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" --build out/cfg
-exit /b %ERRORLEVEL%
